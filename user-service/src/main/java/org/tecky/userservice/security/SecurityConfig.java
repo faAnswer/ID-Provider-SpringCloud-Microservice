fc77c2e52573f550f98335e25ce0d88bcf0e9486
faAnswer <oscarogc@gmail.com>
2022-11-03 00:41:31 +0800

    202211020041

--- user-service/src/main/java/org/tecky/userservice/security/SecurityConfig.java
+++ user-service/src/main/java/org/tecky/userservice/security/SecurityConfig.java
@@ -3,17 +3,14 @@ package org.tecky.userservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
-    public HttpSecurity httpSecurity;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http.authorizeRequests()
                 .antMatchers("/**").hasRole("USER").and().formLogin().and().build();
