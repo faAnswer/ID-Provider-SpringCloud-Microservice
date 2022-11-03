bef89a18029efae8e7cae7a0ecbd4b7f9f82be30
ECSCloud <tecky23project@gmail.com>
2022-11-03 11:37:18 +0800

    202211031137

---
+++ uaa-service/src/main/java/org/tecky/uaaservice/security/config/AuthorizationServerConfig.java
@@ -0,0 +1,26 @@
+package org.tecky.uaaservice.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.getSharedObject(OAuth2AuthorizationServerConfigurer.class);
+
+        http.authorizeRequests()
+                .antMatchers("/*").permitAll()
+                .and().csrf().disable();
+
+        return http.build();
+    }
+}
